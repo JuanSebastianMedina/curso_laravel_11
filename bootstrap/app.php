828663641f848673d02729cf37242906b90f65bd
Juanse <jmedinapineros@gmail.com>
2025-02-16 20:16:15 -0500

    Modelos, relaciones, migraciones, seeders y factories

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,12 +3,18 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        // incorpora una ruta para task
+        then:function(){
+            Route::prefix('tasks')
+                ->group(base_path('routes/task.php'));
+        }
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,31 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Comment;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Database\Seeders\CommentSeeder;
+use Database\Seeders\UsersSeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     * 
+     * User::factory()->create([
+     * 'name' => 'Test User',
+     * 'email' => 'test@example.com',
+     * ]);
+     * 
+     * $this->call([
+     * //UsersSeeder::class,
+     * CommentSeeder::class,
+     * ]);
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::factory(10)->create();
+        Comment::factory(12)->create();
     }
 }
